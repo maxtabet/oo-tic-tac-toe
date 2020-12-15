4903ac73f4e39acc1337ee2d7999f8d7da530e84
Maximilian Tabet <maxtabet@gmail.com>
2020-12-15 17:00:29 -0500

    commit tree 95cd98f8cd059b15332756f7fc0287d550cc5eec

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,42 @@
+class TicTacToe
+
+    WIN_COMBINATIONS = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+    ]
+
+    def initialize(board = nil)
+        @board = board || Array.new(9, " ")
+      end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(string)
+        string.to_i-1
+    end
+
+    def move(index, token = "X")
+        @board[index] = token
+    end
+
+    def position_taken?(index)
+        @board[index] != " "
+    end
+
+    def valid_move?(index)
+        !position_taken?(index) && index.between?(0,8)
+    end
+
+end
